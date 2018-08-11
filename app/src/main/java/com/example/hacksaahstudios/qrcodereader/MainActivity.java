38d6d49b2e761c92e70758d588490bbb311dd793
Joseph Saah <jsaah@gmu.edu>
2018-08-11 18:55:46 -0400

    Added UI before scanning

--- app/src/main/java/com/example/hacksaahstudios/qrcodereader/MainActivity.java
+++ app/src/main/java/com/example/hacksaahstudios/qrcodereader/MainActivity.java
@@ -1,59 +1,22 @@
 package com.example.hacksaahstudios.qrcodereader;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-import com.google.zxing.Result;
-
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-
-public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler
+public class MainActivity extends AppCompatActivity
 {
-    private static final int CAMERA_REQUEST_CODE = 0;
-
-    private ZXingScannerView zXingScannerView;
 
     @Override protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
-        zXingScannerView = new ZXingScannerView(this);
-        setContentView(zXingScannerView);
-
-        askPermissions(Manifest.permission.CAMERA, CAMERA_REQUEST_CODE);
-
-        zXingScannerView.setResultHandler(this);
-        zXingScannerView.startCamera();
+        setContentView(R.layout.activity_main);
     }
 
-    @Override public void handleResult(Result result)
+    public void openScanner(View view)
     {
-        Intent openWebView = new Intent(this, WebViewActivity.class);
-        openWebView.putExtra("url", result.getText());
-        startActivity(openWebView);
-    }
-
-    @Override protected void onPause()
-    {
-        super.onPause();
-        zXingScannerView.stopCamera();
-    }
-
-    @Override protected void onResume()
-    {
-        super.onResume();
-        zXingScannerView.setResultHandler(this);
-        zXingScannerView.startCamera();
-    }
-
-    private void askPermissions(String permission, int requestCode)
-    {
-        if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this, new String[] {permission}, requestCode);
+        Intent openScannerActivity = new Intent(this, ScannerActivity.class);
+        startActivity(openScannerActivity);
     }
 }
---
+++ app/src/main/java/com/example/hacksaahstudios/qrcodereader/ScannerActivity.java
@@ -0,0 +1,59 @@
+package com.example.hacksaahstudios.qrcodereader;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler
+{
+    private static final int CAMERA_REQUEST_CODE = 0;
+
+    private ZXingScannerView zXingScannerView;
+
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        zXingScannerView = new ZXingScannerView(this);
+        setContentView(zXingScannerView);
+
+        askPermissions(Manifest.permission.CAMERA, CAMERA_REQUEST_CODE);
+
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+    @Override public void handleResult(Result result)
+    {
+        Intent openWebView = new Intent(this, WebViewActivity.class);
+        openWebView.putExtra("url", result.getText());
+        startActivity(openWebView);
+    }
+
+    @Override protected void onPause()
+    {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+    @Override protected void onResume()
+    {
+        super.onResume();
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+    private void askPermissions(String permission, int requestCode)
+    {
+        if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[] {permission}, requestCode);
+    }
+}
